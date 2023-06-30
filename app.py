b0fcb113dee795d75e7d0f86767d0e60419ec0f1
unknown <yael.zecharia01@gmail.com>
2023-06-30 13:14:41 +0300

    first step

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    with open("storage.json", "r") as file:
+        blog_posts = json.load(file)
+    return render_template('index.html', posts=blog_posts)
+
+
+if __name__ == '__main__':
+    app.run()
